3f1933ae1bc6064e6a0b0f8567642f16596cc324
Arman Mansuri <armannofficial50@gmail.com>
2026-08-22 12:29:45 +0530

    Update category for Zenith Corporate Tower

--- js/script.js
+++ js/script.js
@@ -369,7 +369,7 @@ if (projectModal) {
 
     "zenith-corporate-tower": {
       title: 'Zenith Corporate Tower',
-      category: 'our crafted spaces',
+      category: 'our crafted spaces, lasting impression',
       location: 'Gurugram, Haryana',
 
       description:
